4e17791d215c1717bed0987c2e63e4b8ab8ad260
yianna <yiannakokalas@gmail.com>
2017-06-25 15:25:00 -0400

    Add basic view with Actor
    - Add basic view by inheriting Gosu::Window into
      Stenomancer.
    
    - Add background png to media.
    
    - Add walking_sprite png to make the sprite animate.
    
    - Add stick_figure.png as a sprite to start implementing an Actor.
    
    - Create an Actor in the view
    
    - Implement instance variables for an actor width, actor height and
      it's x y coordinate. The feature is for the actor to draw on the
      screen at it's center not it's most upper left hand corner.
    - Create a file for an actor class.

--- lib/stenomancer.rb
+++ lib/stenomancer.rb
@@ -1,2 +1,24 @@
-class Stenomancer
+class Stenomancer < Gosu::Window
+  def initialize
+    super 2400, 1800
+
+    # you can optionally set fullscreen
+    # super(800, 600, :fullscreen => true)
+    self.caption = "Stenomancer"
+
+    @background_image = Gosu::Image.new("media/stone_floor.png", :tileable => true)
+    @actor = Gosu::Image.new("media/stick_figure.png")
+    @actor_width = @actor.width
+    @actor_height = @actor.height
+    @x = @actor_width
+    @y = @actor_height
+  end
+
+  def update
+  end
+
+  def draw
+    @background_image.draw(0, 0, 0)
+    @actor.draw(@x - @actor_width / 2, @y - @actor_height / 2, 1)
+  end
 end
