3f771b722cb3365d29fa77019ba643490c1f0d25
yianna <yiannakokalas@gmail.com>
2017-06-29 09:35:34 -0400

    Add keybindings for w,s,e

--- lib/keypress.rb
+++ lib/keypress.rb
@@ -1,18 +1,24 @@
 class Keypress
   def north?
-    Gosu.button_down? Gosu::KB_UP
+    Gosu.button_down?(Gosu::KB_UP) || 
+      Gosu.button_down?(Gosu::KB_T) &&
+      Gosu.button_down?(Gosu::KB_P) &&
+      Gosu.button_down?(Gosu::KB_H)
   end
 
   def east?
-    Gosu.button_down? Gosu::KB_RIGHT
+    Gosu.button_down?(Gosu::KB_RIGHT) || 
+      Gosu.button_down?(Gosu::KB_E)
   end
 
   def south?
-    Gosu.button_down? Gosu::KB_DOWN
+    Gosu.button_down?(Gosu::KB_DOWN) || 
+      Gosu.button_down?(Gosu::KB_S)
   end
 
   def west?
-    Gosu.button_down? Gosu::KB_LEFT
+    Gosu.button_down?(Gosu::KB_LEFT) || 
+      Gosu.button_down?(Gosu::KB_W)
   end
 
 end
